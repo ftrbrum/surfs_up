886c2d51a1d7aebb9928c91475cf0073b22f9a0f
ftrbrum <80592268+ftrbrum@users.noreply.github.com>
2021-06-15 15:10:38 -0400

    mod 9.5.5

--- app.py
+++ app.py
@@ -1,18 +1,63 @@
-# Create a New Python File and Import the Flask Dependency
+# Set Up the Flask Weather App
 
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-# Create a New Flask App Instance
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Set Up the Database
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+# Set Up Flask
+
 app = Flask(__name__)
 
-# Create Flask Routes
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+# Routes 9.5.2 Home page
 
-# Run a Flask App
-set FLASK_APP=app.py
-flask run
+@app.route("/")
 
-# Copy and paste your localhost address into your web browser. Generally, a localhost will look something like this, for context.
-# http://127.0.0.1:5000/
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+# Routes 9.5.3 precipitation page
+
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+# Routes 9.5.4 stations page
+
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
